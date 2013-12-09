1f94a22aa673fe9b1f3922f1ef0320485ac928d6
sinhpah <zacharyhutcherson@gmail.com>
2013-12-09 10:33:27 -0700

    Update MjollnirController.java
    Implemented the Mjollnir Controller and its various methods :D

--- src/edu/neumont/csc130/battleBot/MjollnirController.java
+++ src/edu/neumont/csc130/battleBot/MjollnirController.java
@@ -1,5 +1,60 @@
 package edu.neumont.csc130.battleBot;
 
-public class MjollnirController {
+import lejos.nxt.Motor;
+import lejos.nxt.MotorPort;
+import lejos.nxt.NXTRegulatedMotor;
 
+public class MjollnirController {
+	NXTRegulatedMotor hammerMotor;
+	public static final int OVER_POWER = 1100;
+	
+	public MjollnirController(){
+		hammerMotor = new NXTRegulatedMotor(MotorPort.A);
+		hammerMotor.setSpeed(OVER_POWER);
+	}
+	
+	public void setSpeed(int motorSpeed){
+		hammerMotor.setSpeed(motorSpeed);
+	}
+	
+	public void stopHammerMovement(){
+		hammerMotor.stop();
+	}
+	
+	public int getSpeed(){
+		return hammerMotor.getSpeed();
+	}
+	
+	public void attack(){
+		moveClockwise();
+		moveCounterClockwise();
+	}
+
+	public boolean isHammerMoving(){
+		return hammerMotor.isMoving();
+	}
+	
+	private void moveCounterClockwise() {
+		Motor.A.backward();
+		try {
+			Thread.sleep(300);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Was Moveing CounterClockwise");
+		}
+		Motor.A.stop();
+	}
+
+	private void moveClockwise() {
+		Motor.A.forward();
+		try {
+			Thread.sleep(300);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Was Moveing Clockwise");
+		}
+		Motor.A.stop();
+	}
 }
