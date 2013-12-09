452ace502bbe9bf3753f47be577528c44fc149a6
rantion <raychacha1234@gmail.com>
2013-12-08 18:37:56 -0700

    Update MotorController.java
    let me know if there need to be any changes

--- src/edu/neumont/csc130/battleBot/MotorController.java
+++ src/edu/neumont/csc130/battleBot/MotorController.java
@@ -1,5 +1,102 @@
 package edu.neumont.csc130.battleBot;
+  
+import lejos.nxt.MotorPort;
+import lejos.nxt.NXTMotor;
 
 public class MotorController {
 
+
+    private NXTMotor leftMotor;
+    private NXTMotor rightMotor;
+    private NXTMotor hammerMotor;
+    private final int turning = 35;
+    private final int forward = 40;
+    private final int fullPower = 100;
+    private final int halfPower = 50;
+    private final int backward = 30;
+
+    public MovementController(){
+        leftMotor = new NXTMotor(MotorPort.C);
+        rightMotor = new NXTMotor(MotorPort.A);
+        stopBoth();
+    }
+
+    public void turnLeft(){
+        rightMotor.setPower(turning);
+        rightMotor.forward();
+    }
+
+    public void turnRight(){
+        leftMotor.setPower(turning);
+        leftMotor.forward();
+    }
+
+    public void moveForward(){
+        rightMotor.setPower(forward);
+        leftMotor.setPower(forward);
+        rightMotor.forward();
+        leftMotor.forward();
+    }
+
+    public void moveForwardFullSpeed(){
+        rightMotor.setPower(fullPower);
+        leftMotor.setPower(fullPower);
+        rightMotor.forward();
+        leftMotor.forward();
+    }
+
+    public void moveBackward(){
+        rightMotor.setPower(backward);
+        leftMotor.setPower(backward);
+        rightMotor.backward();
+        leftMotor.backward();
+    }
+
+    public void stopBoth(){
+        rightMotor.stop();
+        leftMotor.stop();
+    }
+
+    public void hammerFullSpeed(){
+       while(true){
+        try{
+        hammerMotor.setPower(fullPower);
+        hammerMotor.forward();
+            Thread.sleep(500);
+        hammerMotor.backward();
+            Thread.sleep(500);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+       }
+
+    }
+
+    public void hammerHalfSpeed(){
+        while(true){
+            try{
+                hammerMotor.setPower(halfPower);
+                hammerMotor.forward();
+                Thread.sleep(700);
+                hammerMotor.backward();
+                Thread.sleep(700);
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public void turnAround(){
+        try{
+            turnLeft();
+            Thread.sleep(2000);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        stopBoth();
+    }
 }
